68443da1b26ea3613ba6bef03af8342191cf7d93
Ratys <ratysz@gmail.com>
2018-05-29 23:27:38 +0300

    Switched to `fern` for logging.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,35 @@
 #![windows_subsystem = "windows"]
+extern crate chrono;
 extern crate concord_core;
 extern crate config;
+extern crate fern;
 #[macro_use]
 extern crate log;
-extern crate simplelog;
 extern crate systray;
 extern crate tokio;
 extern crate tokio_threadpool;
 
 use concord_core::*;
-use simplelog::TermLogger;
 
 mod systray_app;
-use systray_app::SysTray;
 
 fn main() {
-    TermLogger::init(simplelog::LevelFilter::Trace, simplelog::Config::default()).unwrap();
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "[{}][{:<5}][{}] {}",
+                chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
+                record.level().to_string(),
+                record.target(),
+                message
+            ))
+        })
+        .level_for("tokio_threadpool", log::LevelFilter::Debug)
+        .level(log::LevelFilter::Trace)
+        .chain(std::io::stdout())
+        .apply()
+        .unwrap();
+
     let config = {
         let mut config = config::Config::default();
         config.merge(config::File::with_name("config")).unwrap();
@@ -24,7 +38,7 @@ fn main() {
 
     let mut concord = ConcordCore::new(config).expect("Could not initialize Concord!");
     concord
-        .initialize_protocol(SysTray)
+        .initialize_protocol(systray_app::SysTray)
         .expect("Couldn't initialize SysTray!");
 
     /*#[cfg(feature = "discord_protocol")]
